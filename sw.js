7561147a6662e3797c25a4fef48ad98573d33d2d
ragnarokdrakkar-rgb <ragnarok.drakkar@gmail.com>
2026-07-10 22:25:59 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* Workout Tracker service worker v6.0.0 */
-const CACHE_NAME = 'workout-tracker-v6.0.0';
+/* Workout Tracker service worker v6.0.1 */
+const CACHE_NAME = 'workout-tracker-v6.0.1';
 const CORE_FILES = [
   './',
   './index.html',
