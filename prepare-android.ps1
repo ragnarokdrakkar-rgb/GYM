7fb0493b7d84f10054ab51dd167b9e945ea52ba7
Kemal Jazavac <kemal.jazavac@gmail.com>
2026-07-13 00:06:50 +0200

    Release v1.0.14

--- prepare-android.ps1
+++ prepare-android.ps1
@@ -12,6 +12,7 @@ if (-not (Test-Path -LiteralPath $IndexFile)) {
 $wwwDirectory = Split-Path -Parent $IndexFile
 $updateScriptFile = Join-Path $wwwDirectory 'js\app-update.js'
 $restNotificationFile = Join-Path $wwwDirectory 'js\rest-native-notifications.js'
+$uiPatchFile = Join-Path $wwwDirectory 'js\app-ui-patch-v1.js'
 
 if (-not (Test-Path -LiteralPath $updateScriptFile)) {
     throw "Update skripta ni najdena: $updateScriptFile"
@@ -19,6 +20,10 @@ if (-not (Test-Path -LiteralPath $updateScriptFile)) {
 
 if (-not (Test-Path -LiteralPath $restNotificationFile)) {
     throw "Native timer skripta ni najdena: $restNotificationFile"
+}
+
+if (-not (Test-Path -LiteralPath $uiPatchFile)) {
+    throw "UI patch skripta ni najdena: $uiPatchFile"
 }
 
 $content = [System.IO.File]::ReadAllText($IndexFile)
@@ -71,7 +76,8 @@ $content = [regex]::Replace(
 
 $scriptTags = @(
     '<script src="js/app-update.js"></script>',
-    '<script src="js/rest-native-notifications.js"></script>'
+    '<script src="js/rest-native-notifications.js"></script>',
+    '<script src="js/app-ui-patch-v1.js"></script>'
 )
 
 foreach ($scriptTag in $scriptTags) {
